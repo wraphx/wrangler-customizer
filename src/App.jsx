9ecee2ca5dea34cba942abe3fbdf5fc01f920e98
Ty Byers <tyrone@wraphx.com>
2024-09-05 12:08:30 -0400

    updated Configurator

--- src/App.jsx
+++ src/App.jsx
@@ -17,36 +17,18 @@ export default function App() {
         <Canvas shadows camera={{ position: [0, 0, 20], fov:40 }}>
           <color attach="background" args={['grey']} />
           <Suspense fallback={null}>
-          <Wrangler scale={2.3} position={[0, -1.1, 0]} />
+          <Wrangler scale={2.3} position={[0, 0, 0]} />
           </Suspense>
           <hemisphereLight intensity={0.5} />
-          <ContactShadows resolution={1024} frames={1} position={[0, -1.16, 0]} scale={15} blur={0.5} opacity={0.5} far={20} />
-          <mesh scale={4} position={[3, -1.161, -1.5]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-            <ringGeometry args={[0.9, 1, 4, 1]} />
-            <meshStandardMaterial color="white" roughness={0.75} />
-          </mesh>
-          <mesh scale={4} position={[-3, -1.161, -1]} rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}>
-            <ringGeometry args={[0.9, 1, 3, 1]} />
-            <meshStandardMaterial color="white" roughness={0.75} />
-          </mesh>
-          <ambientLight intensity={0.7} />
-          <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <ContactShadows position={[0, 0, 0.0]} opacity={0.75} scale={20} blur={2.5} far={12.8} />
+        <ContactShadows resolution={1024} frames={1} position={[0, 0, 0]} scale={20} blur={1.5} opacity={0.5} far={25} />
+        
+        <ambientLight intensity={0.7} />
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
 
-          {/* <Environment preset="city" /> */}
-          <Environment resolution={512}  >
-            {/* Ceiling */}
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -9]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -6]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, -3]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 0]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 3]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 6]} scale={[10, 1, 1]} />
-            <Lightformer intensity={2} rotation-x={Math.PI / 2} position={[0, 4, 9]} scale={[10, 1, 1]} />
-            {/* Sides */}
-            <Lightformer intensity={20} rotation-y={Math.PI / 2} position={[-50, 2, 0]} scale={[100, 2, 1]} />
-            <Lightformer intensity={20} rotation-y={-Math.PI / 2} position={[50, 2, 0]} scale={[100, 2, 1]} />
-            {/* Key */}
-            <Lightformer form="ring" color="gray" intensity={10} scale={2} position={[10, 5, 10]} onUpdate={(self) => self.lookAt(0, 0, 0)} />
+        {/* <Environment preset="city" /> */}
+        <Environment resolution={1024} files="/st_peters_square_night_2k.hdr" background ground={{ height: 15, radius: 80, scale: 50 }} backgroundBlurriness={50} backgroundIntensity={10}  environmentIntensity={30}>
+           
           </Environment>
           <ContactShadows position={[0, -0.8, 0.0]} opacity={0.75} scale={10} blur={2.5} far={0.8} />
           {/* <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={true} enablePan={false} /> */}
